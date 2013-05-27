83f5664c7800264878b2b51afc82846ed8842d8f
rafan1 <Xman42@seznam.cz>
2013-05-27 14:22:20 +0200

    zkusebni commit

---
+++ pointers3.pl
@@ -0,0 +1,23 @@
+#!/usr/bin/perl -w
+
+$promena = "na to ";
+$promena1 = "nejdem ";
+$jmenoProm = "promena";
+print "${$jmenoProm}jdem\n";
+#$arrayref = [1, 2, 3];
+$text = [10, 11,12];
+$arrayref = ${"text"};
+print "Je to @{$text}\n";
+
+
+exit;
+
+while (<>){
+	$a = $_;
+	@b = split;
+	print @b, "\n";
+	#push @matrix, [$a];
+}
+#$, = ' ';
+
+#print @$matrix, "\n";
